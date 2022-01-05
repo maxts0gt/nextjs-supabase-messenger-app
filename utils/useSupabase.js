62190e8243f99592899d906a8c9ed4c6748b7615
Max <bazarsadtsogtjargal@gmail.com>
2022-01-05 16:30:09 +0900

    track current user

--- utils/useSupabase.js
+++ utils/useSupabase.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
 
 const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -7,13 +7,39 @@ const supabase = createClient(
 );
 
 const useSupabase = () => {
+	const [currentUser, setCurrentUser] = useState(null);
 	const [session, setSession] = useState(supabase.auth.session());
 
 	supabase.auth.onAuthStateChange(async (_event, session) => {
 		setSession(session);
 	});
 
-	return { session, supabase };
+	useEffect(async () => {
+		const getCurrentUser = async () => {
+			if (session?.user.id) {
+				const { data: currentUser } = await supabase
+					.from('user')
+					.select('*')
+					.eq('id', session.user.id);
+
+				if (currentUser.length) {
+					const foundUser = currentUser[0];
+					await supabase
+						.from(`user:id=eq.${foundUser.id}`)
+						.on('UPDATE', (payload) => {
+							setCurrentUser(payload.new);
+						})
+						.subscribe();
+					return foundUser;
+				} else {
+					return null;
+				}
+			}
+		};
+		const foundUser = await getCurrentUser();
+		setCurrentUser(foundUser);
+	}, [session]);
+	return { currentUser, session, supabase };
 };
 
 export default useSupabase;
