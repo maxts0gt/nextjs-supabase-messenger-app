d721db85bac3f39d121367531d67e1a8754d4fed
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-01-05 13:30:07 +0900

    initial repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import useSupabase from '../utils/useSupabase';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
